b5369137aa92725b69dd24d480cb8a785ecbc18a
love-sick <ssajad2153@gmail.com>
2026-09-21 07:50:02 +0330

    Initial commit: Clock widget v1.0.0

---
+++ ClockWidget.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using WDesk.Core;
+using WDesk.Widgets.Clock.Style;
+
+namespace WDesk.Widgets.Clock;
+
+public class ClockWidget : WidgetBase
+{
+    public override WidgetMetadata Metadata { get; } = new()
+    {
+        Id = "clock",
+        NameKey = "widget.clock.name",
+        DescriptionKey = "widget.clock.desc",
+        Category = WidgetCategory.Time,
+        Icon = "\uE823",
+        Author = "WDesk Team",
+        Version = "1.0.0",
+        DefaultWidth = 240,
+        DefaultHeight = 180,
+        HasSettings = false
+    };
+
+    public override FrameworkElement CreateView(PlacedWidget instance)
+    {
+        return new Style1().Build(instance);
+    }
+}
